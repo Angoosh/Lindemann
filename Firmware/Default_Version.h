ac3e089b227865d75e42a06231ce87bbd166b4dc
Angoosh <kaplanantonin@gmail.com>
2017-12-07 21:07:08 +0100

    Re-Upload
    Re-Upload of modified Marlin 1.0.2+
    New version 1.0.3.1

---
+++ Firmware/Default_Version.h
@@ -0,0 +1,14 @@
+/*
+ * This file is a placeholder for a file which could be distributed in an archive
+ * It takes the place of an automatically created "_Version.h" which is generated during the build process
+ */
+
+// #error "You must specify the following parameters related to your distribution"
+
+#if true
+  #define SHORT_BUILD_VERSION "1.0.3.1"
+  #define DETAILED_BUILD_VERSION "1.0.3 SPSE Development Version"
+  #define STRING_DISTRIBUTION_DATE "2017-12-7 00:00"
+  // It might also be appropriate to define a location where additional information can be found
+  #define SOURCE_CODE_URL  "http://github.com/Angoosh/Lindemann"
+#endif
---
+++ README.md.txt
@@ -0,0 +1,4 @@
+Modified marlin firmware.
+
+From 1.0.2+
+To 1.0.3.1
