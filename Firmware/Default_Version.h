418d01af7aac6d9791456f7b46b184e29b83b9b0
Angoosh <kaplanantonin@gmail.com>
2018-01-18 16:55:43 +0100

    updated version

--- Firmware/Default_Version.h
+++ Firmware/Default_Version.h
@@ -6,9 +6,9 @@
 // #error "You must specify the following parameters related to your distribution"
 
 #if true
-  #define SHORT_BUILD_VERSION "1.0.3.1"
+  #define SHORT_BUILD_VERSION "1.0.3.2"
   #define DETAILED_BUILD_VERSION "1.0.3 SPSE Development Version"
-  #define STRING_DISTRIBUTION_DATE "2017-12-7 00:00"
+  #define STRING_DISTRIBUTION_DATE "2018-1-18 16:55"
   // It might also be appropriate to define a location where additional information can be found
   #define SOURCE_CODE_URL  "http://github.com/Angoosh/Lindemann"
 #endif
